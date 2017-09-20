d969609742bbd0f5c2ccc5c9d6a0530658517abd
Cris <muscaiu@yahoo.com>
2017-09-20 22:14:11 +0300

    testing scss

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-// import './index.css'
-import './sass.scss'
+import './index.scss'
 
 const Header = () => (
   <div
