36d1f14bf2a4e68dd04e9e3270bdfd0ad90b2a2d
Cris <muscaiu@yahoo.com>
2017-09-20 22:09:13 +0300

    add and import sass

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+// import './index.css'
+import './sass.scss'
 
 const Header = () => (
   <div
